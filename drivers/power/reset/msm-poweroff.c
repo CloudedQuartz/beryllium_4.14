7f009bcaf46c4776fef0f008aecb32df49fb9bec
MrArtemSid <artem.sid.key@gmail.com>
2021-01-02 20:27:47 +0400

    Revert "reset: msm-poweroff: Add support for Sony Seine platform"
    This reverts commit 27ed0afd38a5f52dcfb3df5a1c4ebe57d5b0123d.

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -55,11 +55,10 @@
 #endif
 #if defined(CONFIG_ARCH_SONY_YOSHINO) || defined(CONFIG_ARCH_SONY_NILE) || \
     defined(CONFIG_ARCH_SONY_TAMA) || defined(CONFIG_ARCH_SONY_GANGES) || \
-    defined(CONFIG_ARCH_SONY_KUMANO) || defined(CONFIG_ARCH_SONY_SEINE)
+    defined(CONFIG_ARCH_SONY_KUMANO)
  #define TARGET_SOMC_XBOOT
 #if defined(CONFIG_ARCH_SONY_NILE) || defined(CONFIG_ARCH_SONY_TAMA) || \
-    defined(CONFIG_ARCH_SONY_GANGES) || defined(CONFIG_ARCH_SONY_KUMANO) || \
-    defined(CONFIG_ARCH_SONY_SEINE)
+    defined(CONFIG_ARCH_SONY_GANGES) || defined(CONFIG_ARCH_SONY_KUMANO)
  #define TARGET_SOMC_XBOOT_FEATURE_AB
 #endif
 #endif
